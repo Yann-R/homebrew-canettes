69403fc4eb428a947c9b24975d435c42c9a592ac
Yann-R <yann.ricquebourg@irisa.fr>
2020-12-13 16:55:26 +0100

    Create dnd-me.rb

---
+++ Casks/dnd-me.rb
@@ -0,0 +1,25 @@
+cask "dnd-me" do
+  version "1.1.2"
+  sha256 "d57e89a8c6c569fd9fc3daa2335011253694b7b5b6521b33e744360a42b4009b"
+
+  url "https://dl.devmate.com/com.runtimesharks.dndme/DNDMe.dmg"
+  name "DND Me"
+  homepage "https://runtimesharks.com/projects/dnd-me"
+
+  depends_on macos: "< :big_sur"
+
+  app "DND Me.app"
+
+  uninstall login_item: "DND Me"
+
+  zap trash: [
+    "~/Library/Application Support/DND Me",
+    "~/Library/Caches/com.runtimesharks.dndme",
+    "~/Library/Preferences/com.runtimesharks.dndme.plist",
+  ]
+  
+  caveats <<~EOS
+    • Due to system limitations, DND Me does not work if Do Not Disturb is scheduled in System Preferences -> Notifications.
+    • Currently not working on macOS Big Sur.
+  EOS
+end
