1f24222a1b68e793cec7ea47277e1853f52008e8
Yann-R <yann.ricquebourg@irisa.fr>
2020-12-19 18:02:53 +0100

    Update a2ps-utf8.rb

--- Formula/a2ps-utf8.rb
+++ Formula/a2ps-utf8.rb
@@ -1,7 +1,7 @@
 class A2psUtf8 < Formula
   desc "Wrapper around a2ps to accept input files in UTF8 (using iconv to convert)"
-  homepage "http://matthieu-moy.fr/utils/"
-  url "http://matthieu-moy.fr/utils/a2ps-utf8"
+  homepage "https://matthieu-moy.fr/utils/"
+  url "https://matthieu-moy.fr/utils/a2ps-utf8"
   version "2005.1.21"
   sha256 "84e43a23b2abe23cf4c46cf393e0d9afa1a36dbb7eb827cbde16ab9524fb6f41"
 
