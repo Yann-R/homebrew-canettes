1386ed87d4a58118f8c5b66a9c1ca90a6112510c
Yann-R <yann.ricquebourg@irisa.fr>
2020-12-20 11:04:30 +0100

    Update dnd-me.rb

--- Casks/dnd-me.rb
+++ Casks/dnd-me.rb
@@ -1,10 +1,9 @@
 cask "dnd-me" do
   version "1.1.2"
-  sha256 "d57e89a8c6c569fd9fc3daa2335011253694b7b5b6521b33e744360a42b4009b"
+  sha256 :no_check
 
   url "https://dl.devmate.com/com.runtimesharks.dndme/DNDMe.dmg",
       verified: "https://dl.devmate.com/"
-  appcast "https://runtimesharks.com/projects/dnd-me"
   name "DND Me"
   desc "Activate Do Not Disturb for a few hours"
   homepage "https://runtimesharks.com/projects/dnd-me"
@@ -20,7 +19,7 @@ cask "dnd-me" do
     "~/Library/Caches/com.runtimesharks.dndme",
     "~/Library/Preferences/com.runtimesharks.dndme.plist",
   ]
-  
+
   caveats <<~EOS
     • Due to system limitations, DND Me does not work if Do Not Disturb is scheduled in System Preferences -> Notifications.
     • Currently not working on macOS Big Sur.
