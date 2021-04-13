2b18bd6cf998e476d2609d745932ac22defca621
Yann-R <yann.ricquebourg@irisa.fr>
2021-04-13 20:20:25 +0200

    Add touche.rb

---
+++ Casks/touche.rb
@@ -0,0 +1,19 @@
+cask "touche" do
+  version "1.1.4"
+  sha256 "275a2885214befd088fe20707f306d3ad1743cf8181334ed4ef976391fff6c7f"
+
+  url "https://red-sweater.com/touche/Touche#{version}.zip"
+  name "Touché"
+  desc "Touch Bar for everyone (Simulate, show and hide, take screen captures)"
+  homepage "https://redsweater.com/touche/"
+
+  depends_on macos: ">= :sierra"
+
+  app "Touché.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.red-sweater.touche",
+    "~/Library/Containers/com.red-sweater.touche",
+    "~/Library/Preferences/com.red-sweater.touche.plist",
+  ]
+end
