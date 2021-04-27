cc89e09cd89489431d794c3b4e776a21d2305b45
Yann-R <yann.ricquebourg@irisa.fr>
2021-04-27 16:53:57 +0200

    Update eclipse-clp.rb

--- Formula/eclipse-clp.rb
+++ Formula/eclipse-clp.rb
@@ -5,6 +5,16 @@ class EclipseClp < Formula
   version "6.1-164"
   sha256 "3946b7a3b3ed2f94b6cafe26e323ed848e4032da814cc7bd303d0d48ed326e69"
 
+  livecheck do
+    url "http://eclipseclp.org/Distribution/CurrentRelease/"
+    regex(%r{(\d+\.\d+_\d+).*MacOS}i)
+    # e.g. 6.1_164 x86_64_macosx Intel-64bit-MacOS
+    # and put '-' instead of '_' (not accepted by brew in versions)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match&.first&.sub("_", "-") }
+    end
+  end
+
   resource "eclipse-doc" do
     url "http://eclipseclp.org/Distribution/CurrentRelease/6.1_164%20x86_64_macosx%20Intel-64bit-MacOS/eclipse_doc.tgz"
     sha256 "064b1e46d83150953af259346e2e3f97a0f96e3f1493829eea72978577dc6a2c"
