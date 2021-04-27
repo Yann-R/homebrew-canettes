cdd6734de990e8ebe38bd17d4b6fa7c38e80b219
Yann-R <yann.ricquebourg@irisa.fr>
2021-04-27 12:08:45 +0200

    Create akku.rb

---
+++ Casks/akku.rb
@@ -0,0 +1,33 @@
+cask "akku" do
+  version "0.1.0-beta.11"
+  sha256 "6e6a55ea5f70d060901d87fa8aa4d66b5f2a486760905e9946e277c01bf661f3"
+
+  url "https://github.com/jariz/Akku/releases/download/#{version}/Akku.dmg",
+     verified: "https://github.com/jariz/Akku/releases/download"
+
+  name "Akku"
+  desc "The missing macOS bluetooth headset battery indicator app"
+  homepage "https://akku.software/"
+
+  livecheck do
+    url "https://github.com/jariz/akku/releases"
+    strategy :page_match
+    regex(%r{href=.*?/releases/tag/(\d+(?:\.\d+)*(?:-beta\.\d+))}i)
+    #e.g. https://github.com/jariz/Akku/releases/tag/0.1.0-beta.11
+  end
+
+  app "Akku.app"
+  
+  uninstall login_item: "Akku"
+
+  zap trash: [
+    "Library/PrivilegedHelperTools/io.jari.AkkuHelper",
+    "/Library/LaunchDaemons/io.jari.AkkuHelper.plist",
+    "~/Library/Cache/Akku",
+    "~/Library/Logs/Akku.log"
+  ]
+  
+  caveats <<~EOS
+    Install issue on Catalina, hanging on after 1st install of the helper (and needing [Force Quit]): https://github.com/jariz/Akku/issues/33
+  EOS
+end
