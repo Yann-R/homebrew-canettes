15b256208e268d4e00f6f8c76e978ef80434e851
Yann-R <yann.ricquebourg@irisa.fr>
2021-06-08 13:43:41 +0200

    Update a2ps-utf8.rb

--- Formula/a2ps-utf8.rb
+++ Formula/a2ps-utf8.rb
@@ -5,6 +5,23 @@ class A2psUtf8 < Formula
   version "2005.1.21"
   sha256 "84e43a23b2abe23cf4c46cf393e0d9afa1a36dbb7eb827cbde16ab9524fb6f41"
 
+  livecheck do
+    url :homepage # NOTE: this page is not encoded in UTF-8
+    regex(/Derni.re mise . jour : (?<dofw>\S*) (?<day>\d{1,2}) (?<month>\S+) (?<year>\d{4,4})/i)
+    # e.g. Dernière mise à jour : vendredi 21 janvier 2005.
+    strategy :page_match do |page, regex|
+      # Converts date-based version from French texts to YYYY.M.D
+      page.match(regex) do |match|
+        nofm = match[:month].to_s
+                            .sub(/jan\S*/, "1").sub(/f.v\S*/, "2").sub(/mar\S*/, "3")
+                            .sub(/avr\S*/, "4").sub(/mai\S*/, "5").sub(/juin\S*/, "6")
+                            .sub(/juil\S*/, "7").sub(/ao\S*/, "8").sub(/sep\S*/, "9")
+                            .sub(/oct\S*/, "10").sub(/nov\S*/, "11").sub(/d.c\S*/, "12")
+        "#{match[:year]}.#{nofm}.#{match[:day]}"
+      end
+    end
+  end
+
   depends_on "a2ps"
   uses_from_macos "libiconv"
 
