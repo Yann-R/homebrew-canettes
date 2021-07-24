6990c9f1dbcd9fe818c14451db3894042d43dd55
Yann-R <yann.ricquebourg@irisa.fr>
2021-07-24 15:08:41 +0200

    Update and rename rar.rb to rar-unrar.rb

--- Formula/rar-unrar.rb
+++ Formula/rar-unrar.rb
@@ -1,4 +1,4 @@
-class Rar < Formula
+class RarUnrar < Formula
   desc "Archive manager for RAR and other formats (with man pages)"
   homepage "http://www.rarlab.com/"
   url "https://www.rarlab.com/rar/rarosx-6.0.2.tar.gz"
@@ -12,15 +12,18 @@ class Rar < Formula
     # e.g. rarosx-6.0.2b1.tar.gz
   end
 
-  head do
+  head do # To get the eventual beta version
     url "https://www.rarlab.com/rar/rarosx-6.0.2b1.tar.gz"
     sha256 "3c968cc4b77a71fff955bfb2e31ec71fec688ca4fb7b8fd791f6e057809b808d"
   end
 
   bottle :unneeded
 
+  option "without-unrar", "Don't install unrar"
+  # option "with-version",  "Add version number as suffix of executables"
+
   # conflicts_with formula: "unrar" # Removed by https://github.com/Homebrew/homebrew-core/pull/66609
-  conflicts_with cask: "rar"
+  # conflicts_with cask: "rar"
 
   resource "man-rar" do
     url "http://manned.org/rar/e007f5f2/src"
@@ -33,22 +36,29 @@ class Rar < Formula
   end
 
   def install
-    # Creates convenience symlinks (with version numbers to avoid conflicts)
-    File.symlink "rar", "rar-#{version}"
-    File.symlink "unrar", "unrar-#{version}"
+    # Renames binaries with version numbers to avoid conflicts
+    File.rename "rar", "rar-#{version}"
+    File.rename "unrar", "unrar-#{version}"
+
+    # Creates convenience symlinks
+    File.symlink "rar-#{version}", "rar"
+    File.symlink "unrar-#{version}", "unrar"
 
     # Installs each command
     bin.install "rar-#{version}", "unrar-#{version}" # Avoids any conflict
-    bin.install "rar", "unrar" # Could conflict with other installs
+    bin.install "rar"
+    bin.install "unrar" if build.with? "unrar"
 
-    # Installs documentations and licence informations
+    # Installs documentations and license informations
     # doc.install Dir["*.*"] # Replaced by following lines, more selective
     lib.install "default.sfx"
     etc.install "rarfiles.lst" # As recommended in rar.txt
     doc.install "acknow.txt", "license.txt", "order.htm", "rar.txt", "readme.txt", "whatsnew.txt"
+
+    # Installs man pages (from Debian)
     resource("man-rar").stage { man1.install "src" => "rar.1" }
     resource("man-unrar").stage { man1.install "src" => "unrar.1" }
-    inreplace "#{man1}/rar.1", "/usr/share/doc/rar", "#{HOMEBREW_PREFIX}/share/doc/rar"
+    inreplace "#{man1}/rar.1", "/usr/share/doc/rar", "#{HOMEBREW_PREFIX}/share/doc/#{name}"
   end
 
   test do
