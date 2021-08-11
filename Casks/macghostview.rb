8ad859c7a4331aadb41b532d497363fc63875b2f
Yann-R <yann.ricquebourg@irisa.fr>
2021-08-11 10:28:38 +0200

    Update macghostview.rb

--- Casks/macghostview.rb
+++ Casks/macghostview.rb
@@ -13,17 +13,44 @@ cask "macghostview" do
     regex(/MacGhostView (\d+(?:\.\d+)*)/i)
   end
 
+  shimscript = "#{staged_path}/gv_xdvi-motif_xpdf/gv.sh"
   app "MacGhostView.app"
-  app "gv_xdvi-motif_xpdf/Dropscript-gv.app"
-  app "gv_xdvi-motif_xpdf/Dropscript-xdvim.app"
-  app "gv_xdvi-motif_xpdf/Dropscript-xpdf.app"
-  binary "gv_xdvi-motif_xpdf/gv"
+  app "gv_xdvi-motif_xpdf/Dropscript-gv.app", target: "MacGhostView/Dropscript-gv.app"
+  app "gv_xdvi-motif_xpdf/Dropscript-xdvim.app", target: "MacGhostView/Dropscript-xdvim.app"
+  app "gv_xdvi-motif_xpdf/Dropscript-xpdf.app", target: "MacGhostView/Dropscript-xpdf.app"
+  binary shimscript, target: "gv"
+  binary "gv_xdvi-motif_xpdf/gv", target: "gv-bin"
   binary "gv_xdvi-motif_xpdf/xdvim for MacTeX 2020/xdvim"
   binary "gv_xdvi-motif_xpdf/xdvim for MacTeX 2020/xdvi-motif"
   binary "gv_xdvi-motif_xpdf/xpdf"
   binary "gv_xdvi-motif_xpdf/xpdf-bin"
   manpage "gv_xdvi-motif_xpdf/gv.1"
   manpage "gv_xdvi-motif_xpdf/xpdf.1"
+
+  preflight do
+    # Creates one more wrapper script to ensure command-line gv uses the supplied Ghostscript
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      # Ensures use of a compatible Ghostscript (supplied in the .app)
+      export PATH=#{appdir}/MacGhostView.app/Contents/Resources/bings:$PATH
+      exec gv-bin "$@"
+    EOS
+  end
+  
+  postflight do
+    # Ensures the path to the installed commands inside Drop scripts.
+    %w[gv xdvim xpdf].each do |script|
+      system_command "sed", 
+                     args: [
+                       "-i",
+                       "",
+                       "s%/usr/local/bin%#{HOMEBREW_PREFIX}/bin%g", 
+                       "/Applications/MacGhostView/Dropscript-#{script}.app/Contents/Resources/script",
+                     ]
+    end
+  end
+
+  uninstall delete: "/Applications/MacGhostView"
 
   zap trash: [
     "~/Library/Saved Application State/com.tkiffe.MacGhostView.savedState",
