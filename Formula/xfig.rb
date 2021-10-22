e297324c96e9e0a906bd386a25701a4047541d05
Yann-R <yann.ricquebourg@irisa.fr>
2021-10-22 15:44:50 +0200

    Bump xfig to version 3.2.8b

--- Formula/xfig.rb
+++ Formula/xfig.rb
@@ -1,8 +1,9 @@
 class Xfig < Formula
   desc "Interactive drawing tool for X"
   homepage "https://mcj.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/mcj/xfig-3.2.8a.tar.xz"
-  sha256 "ba43c0ea85b230d3efa5a951a3239e206d0b033d044c590a56208f875f888578"
+  url "https://downloads.sourceforge.net/project/mcj/xfig-3.2.8b.tar.xz"
+  sha256 "b2cc8181cfb356f6b75cc28771970447f69aba1d728a2dac0e0bcf1aea7acd3a"
+  license "MIT"
 
   livecheck do
     url :stable
@@ -12,8 +13,12 @@ class Xfig < Formula
   depends_on "fig2dev"
   depends_on "ghostscript"
   depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
   depends_on "libx11"
   depends_on "libxaw3d"
+  depends_on "libxi"
+  depends_on "libxpm"
   depends_on "libxt"
 
   def install
@@ -21,12 +26,16 @@ class Xfig < Formula
       --prefix=#{prefix}
       --disable-dependency-tracking
       --disable-silent-rules
+      --with-appdefaultdir=#{etc}/X11/app-defaults
     ]
 
     system "./configure", *args
+    # "LDFLAGS" argument can be deleted the next release after 3.2.8a. See discussion at
+    # https://sourceforge.net/p/mcj/discussion/general/thread/36ff8854e8/#fa9d.
+    system "make", "LDFLAGS=-ltiff -ljpeg -lpng", "install-strip"
     # Patches one makefile using implicitely libjpeg on macOS
-    inreplace "src/Makefile", "-lXpm", "-lXpm -ljpeg"
-    system "make", "install"
+    #inreplace "src/Makefile", "-lXpm", "-lXpm -ljpeg"
+    #system "make", "install"
   end
 
   test do
