f983de84ba4c287098c8b23a574021b3a06639cc
Yann-R <yann.ricquebourg@irisa.fr>
2022-03-14 20:04:42 +0100

    Update dnd-me1.rb

--- Casks/dnd-me1.rb
+++ Casks/dnd-me1.rb
@@ -1,8 +1,8 @@
 cask "dnd-me1" do
-  version "1.1.3,837:1611389306"
+  version "1.1.3,837,1611389306"
   sha256 "15edcec06d4f0d754c77e1e3aa0773410ecccc0766df3448ad2d638b5f626598"
 
-  url "https://dl.devmate.com/com.runtimesharks.dndme/#{version.after_comma.before_colon}/#{version.after_colon}/DNDMe-#{version.after_comma.before_colon}.dmg",
+  url "https://dl.devmate.com/com.runtimesharks.dndme/#{version.csv.second}/#{version.csv.third}/DNDMe-#{version.csv.second}.dmg",
       verified: "dl.devmate.com/" # Exists also as .zip (zipped dmg)
   name "DND Me"
   desc "Activate Do Not Disturb for a few hours"
@@ -19,7 +19,7 @@ cask "dnd-me1" do
         /DNDMe-(?<version>\d+)\.[dmg|zip]
         .*shortVersionString="(?<short_version>1(?:\.\d+)*)"
       }xi)
-      "#{match[:short_version]},#{match[:version]}:#{match[:tag]}"
+      "#{match[:short_version]},#{match[:version]},#{match[:tag]}"
     end
   end
 
