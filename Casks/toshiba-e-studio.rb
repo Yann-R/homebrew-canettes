b848ae8c50de22ffa575757c6dc544c83c8cbe2f
Yann-R <yann.ricquebourg@irisa.fr>
2022-04-19 12:01:44 +0200

    Update toshiba-e-studio.rb

--- Casks/toshiba-e-studio.rb
+++ Casks/toshiba-e-studio.rb
@@ -1,21 +1,22 @@
 cask "toshiba-e-studio" do
-  version "7.111"
-  sha256 "75ae436dad384e21ae14c540457f3b202229b958ce53475c68b92988da9b8515"
+  version "7.113.0.4"
+  sha256 "256cfedba3fc6500789b1ebcd2a9a4a9cc441557967ea442b134cbca8339e6a4"
 
-  url "https://toshibatec.eu/publicsite-service/resource/download/pseu/en/fea19da2-6b22-41d4-901c-76a352873e2a/8abbae3793dca9cf3b659837ab8958d9/TOSHIBA_e-STUDIO_MacPPD_10.7-__v7.111.zip"
+  url "https://www.toshibatec.eu/publicsite-service/resource/download/pseu/en/82d76edc-94ac-4be3-bff8-5df869227837/a9d700ac68ca348fcd83adea3b3b2bac/TOSHIBA%20e-STUDIO%20MacPPD(10.7.x-11.x)%20v#{version}.zip"
   name "Toshiba e-STUDIO MacPPD"
-  desc "Driver for Multi-Function Printer Toshiba e-STUDIO Series on macOS"
-  homepage "https://www.toshibatec.eu/support/drivers/SearchDriver?searchString=e-studio2505"
+  desc "Drivers (Color & Mono) for Multi-Function Printer Toshiba e-STUDIO Series"
+  homepage "https://www.toshibatec.eu/products/multifunctional-systems-and-printers/"
 
   livecheck do
-    url :homepage
+    url "https://www.toshibatec.eu/products/multifunctional-systems-and-printers/e-studio400ac/"
     strategy :page_match
     regex(/TOSHIBA e-STUDIO MacPPD.*v(\d+(?:\.\d+)*)/i)
     # e.g. TOSHIBA e-STUDIO MacPPD(10.7-) v7.111
   end
-  
+
   # Installs the 1st pkg for Color MFP (in double-sided mode)
   container nested: "MacPPD/OSX10_7-/2-sided_default/TOSHIBA_ColorMFP.dmg.gz"
+
   pkg "TOSHIBA ColorMFP.pkg"
 
   postflight do
@@ -23,10 +24,10 @@ cask "toshiba-e-studio" do
     container nested: "MacPPD/OSX10_7-/2-sided_default/TOSHIBA_MonoMFP.dmg.gz"
     pkg "TOSHIBA MonoMFP.pkg"
   end
-  
-  uninstall pkgutil: "com.toshiba.pde.x7.colormfp"
 
   uninstall_postflight do
     uninstall pkgutil: "com.toshiba.pde.x7.monomfp"
   end
+
+  uninstall pkgutil: "com.toshiba.pde.x7.colormfp"
 end
