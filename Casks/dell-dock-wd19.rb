96e80725947c577a10ffaf979fa2c1b11d9c249f
Yann-R <yann.ricquebourg@irisa.fr>
2022-06-16 15:47:19 +0200

    Bump dell-dock-wd19 to version 01.00.24,A07

--- Casks/dell-dock-wd19.rb
+++ Casks/dell-dock-wd19.rb
@@ -1,15 +1,15 @@
 cask "dell-dock-wd19" do
-  version "01.00.21.01,A06"
+  version "01.00.24,A07"
   sha256 "55ae7b65cc23f803c080a4962e23e2e225368319f1189396930481772ce7f132"
 
-  url "https://dl.dell.com/FOLDER07580718M/1/DellDockingStationFwUp_01.00.21_07222021_WD19.exe"
+  url "https://dl.dell.com/FOLDER08239444M/1/DellDockFirmwarePackage_WD19_WD22_Series_01.00.24.exe"
   name "Dell Docking Station Firmware Update"
-  desc "Dell Dock WD19 Series Firmware Update Utility"
-  homepage "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=wmm77"
+  desc "Dell Dock WD19 & WD22 Series Firmware Update Utility"
+  homepage "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=dptw0&oscode=wt64a&productcode=dell-wd19-130w-dock"
 
   livecheck do
     # Actually not working since Dell website "hides" the subsections contents without manual click :-(
-    url "https://www.dell.com/support/home/fr-fr/product-support/product/dell-wd19-130w-dock/drivers"
+    url "https://www.dell.com/support/home/en-us/product-support/product/dell-wd19-130w-dock/drivers"
     regex(/>(?<num>\d+(:?\.\d+)*), (?<tag>A\d+)</i)
     # e.g. >01.00.21.01, A06<
     strategy :page_match do |page, regex|
