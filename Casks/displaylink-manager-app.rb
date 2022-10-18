a6d4edd61f15475e46ec76b487dcb9d2cdfa6677
Yann-R <yann.ricquebourg@irisa.fr>
2022-10-18 21:23:09 +0200

    Update displaylink-manager-app.rb

--- Casks/displaylink-manager-app.rb
+++ Casks/displaylink-manager-app.rb
@@ -1,8 +1,8 @@
 cask "displaylink-manager-app" do
-  version "1.7.1"
+  version "1.7.1" # and "2022-07" inseted in the url.
   sha256 "32e2c0dce4888895e59e50181f0e15118d8f066abb4568e994878ce0294d422c"
 
-  url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20Manager%20Graphics%20Connectivity#{version}-EXE.pkg"
+  url "https://www.synaptics.com/sites/default/files/exe_files/2022-07/DisplayLink%20Manager%20Graphics%20Connectivity#{version}-EXE.pkg"
   name "DisplayLink Manager Graphics Connectivity"
   desc "DisplayLink solution to connect any display to any computer via USB or Wi-Fi"
   homepage "https://www.synaptics.com/products/displaylink-graphics"
