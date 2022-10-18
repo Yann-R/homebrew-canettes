139eaabb465af54cdccc2da726efb9dbb21a936b
Yann-R <yann.ricquebourg@irisa.fr>
2022-10-18 21:08:18 +0200

    Update displaylink-manager.rb

--- Casks/displaylink-manager.rb
+++ Casks/displaylink-manager.rb
@@ -1,46 +1,24 @@
 cask "displaylink-manager" do
-  version "1.7.1,2022-07"
-  sha256 "32e2c0dce4888895e59e50181f0e15118d8f066abb4568e994878ce0294d422c"
+  version :latest
+  sha256 :no_check
 
-  url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20Manager%20Graphics%20Connectivity#{version.csv.first}-EXE.pkg"
-  name "DisplayLink Manager Graphics Connectivity"
-  desc "DisplayLink solution to connect any display to any computer via USB or Wi-Fi"
+  url "https://www.synaptics.com/products/displaylink-graphics"
+  name "DisplayLink Manager & Login Screen Extension"
+  desc "Cask to install full DisplayLink Manager (App & Extension)"
   homepage "https://www.synaptics.com/products/displaylink-graphics"
 
   livecheck do
-    url "https://www.synaptics.com/products/displaylink-graphics/downloads/macos"
-    strategy :page_match
-    regex(/DisplayLink Manager Graphics Connectivity.*\n.*\n.*Release: (\d+(?:\.\d+)*) \|/i)	# Avoids Alpha or non standard version
-    # regex(/DisplayLink Manager Graphics Connectivity.*\n.*\n.*Release: (\d+(?:\.\d+)*)/i)   	# Any version, maybe ALpha
-    # e.g. <h4>DisplayLink Manager Graphics Connectivity</h4><p>Monterey 12, Big Sur 11</p><p>Release: 1.6.1 | <time datetime="00Z">Feb 23, 2022</time>
+    skip "This cask only installs 2 depending casks"
   end
 
   depends_on macos: ">= :big_sur"
+  depends_on cask: "displaylink-manager-app"
+  depends_on cask: "displaylink-login-screen-ext"
 
-  pkg "DisplayLink Manager Graphics Connectivity#{version.csv.first}-EXE.pkg"
-
-  preflight do
-    system_command "#{HOMEBREW_PREFIX}/bin/brew", args: ["uninstall", "--force", "displaylink-login-screen-ext"]
-  end
-
-  postflight do
-    system_command "#{HOMEBREW_PREFIX}/bin/brew", args: ["install", "displaylink-login-screen-ext"]
-  end
+  stage_only true
 
   uninstall_preflight do
     system_command "#{HOMEBREW_PREFIX}/bin/brew", args: ["uninstall", "--force", "displaylink-login-screen-ext"]
-  end
-
-  uninstall pkgutil: "com.displaylink.displaylinkmanagerapp"
-
-  zap trash: [
-    "~/Library/Application Scripts/com.displaylink.DisplayLinkUserAgent",
-    "~/Library/Containers/com.displaylink.DisplayLinkUserAgent",
-    "~/Library/Group Containers/*.com.displaylink.DisplayLinkShared",
-  ]
-
-  caveats do
-    reboot
-    license "https://www.synaptics.com/products/displaylink-graphics/downloads/macos-#{version.csv.first}"
+    system_command "#{HOMEBREW_PREFIX}/bin/brew", args: ["uninstall", "--force", "displaylink-manager-app"]
   end
 end
