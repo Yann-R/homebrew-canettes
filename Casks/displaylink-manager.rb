215d9bfd3dc5d04fd62abf22f0166845a9052c8d
Yann-R <yann.ricquebourg@irisa.fr>
2022-10-18 20:11:59 +0200

    Bump displaylink-manager to version 1.7.1

--- Casks/displaylink-manager.rb
+++ Casks/displaylink-manager.rb
@@ -1,22 +1,35 @@
-cask "displaylink" do
-  version "1.6.1"
-  sha256 "b38e4c7e69072e1f443c4778477cd69d56aee1430f659c097dcaefb56807ae42"
+cask "displaylink-manager" do
+  version "1.7.1,2022-07"
+  sha256 "32e2c0dce4888895e59e50181f0e15118d8f066abb4568e994878ce0294d422c"
 
-  url "https://www.synaptics.com/sites/default/files/exe_files/2022-02/DisplayLink%20Manager%20Graphics%20Connectivity#{version}-EXE.pkg"
+  url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20Manager%20Graphics%20Connectivity#{version.csv.first}-EXE.pkg"
   name "DisplayLink Manager Graphics Connectivity"
-  desc "Solution of DisplayLink to connect any display to any computer that supports USB or Wi-Fi"
+  desc "DisplayLink solution to connect any display to any computer via USB or Wi-Fi"
   homepage "https://www.synaptics.com/products/displaylink-graphics"
 
   livecheck do
     url "https://www.synaptics.com/products/displaylink-graphics/downloads/macos"
     strategy :page_match
-    regex(/DisplayLink Manager Graphics Connectivity.*\n.*\n.*Release: (\d+(?:\.\d+)*)/i)
+    regex(/DisplayLink Manager Graphics Connectivity.*\n.*\n.*Release: (\d+(?:\.\d+)*) \|/i)	# Avoids Alpha or non standard version
+    # regex(/DisplayLink Manager Graphics Connectivity.*\n.*\n.*Release: (\d+(?:\.\d+)*)/i)   	# Any version, maybe ALpha
     # e.g. <h4>DisplayLink Manager Graphics Connectivity</h4><p>Monterey 12, Big Sur 11</p><p>Release: 1.6.1 | <time datetime="00Z">Feb 23, 2022</time>
   end
 
   depends_on macos: ">= :big_sur"
 
-  pkg "DisplayLink Manager Graphics Connectivity#{version}-EXE.pkg"
+  pkg "DisplayLink Manager Graphics Connectivity#{version.csv.first}-EXE.pkg"
+
+  preflight do
+    system_command "#{HOMEBREW_PREFIX}/bin/brew", args: ["uninstall", "--force", "displaylink-login-screen-ext"]
+  end
+
+  postflight do
+    system_command "#{HOMEBREW_PREFIX}/bin/brew", args: ["install", "displaylink-login-screen-ext"]
+  end
+
+  uninstall_preflight do
+    system_command "#{HOMEBREW_PREFIX}/bin/brew", args: ["uninstall", "--force", "displaylink-login-screen-ext"]
+  end
 
   uninstall pkgutil: "com.displaylink.displaylinkmanagerapp"
 
@@ -25,4 +38,9 @@ cask "displaylink" do
     "~/Library/Containers/com.displaylink.DisplayLinkUserAgent",
     "~/Library/Group Containers/*.com.displaylink.DisplayLinkShared",
   ]
+
+  caveats do
+    reboot
+    license "https://www.synaptics.com/products/displaylink-graphics/downloads/macos-#{version.csv.first}"
+  end
 end
