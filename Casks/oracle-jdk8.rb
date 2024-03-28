eb92f55aee9bd68a559a944a38338277c38d0a3b
Yann-R <yann.ricquebourg@irisa.fr>
2024-03-28 15:15:22 +0100

    Create oracle-jdk8.rb
    Inspired from retired java8 cask

---
+++ Casks/oracle-jdk8.rb
@@ -0,0 +1,67 @@
+cask "oracle-jdk8" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "1.8.0_401" # Release = Java 8 Update 401
+  sha256 arm:   "9211fb9f9fa18dc31e3d272f514d4194ac70fdb7343593c774ff8cb8f68e382f",
+         intel: "fa1f31f40aed887f03eb72e7a8d5c68ab6b533c464c1dd0db6b0bfe6a158cdb2"
+  
+  # version in brew = major.minor.patch, then extract java update number from inside the patch version
+  java_update = version.patch.sub(%r{.*_(\d+).*}, '\1') # e.g. 172 for version 1.8.0_172-b10
+  java_release = version.minor+"u"+java_update # e.g. 8u172
+  # Official URL, requires registering e-mail ID
+  #  e.g. https://download.oracle.com/otn/java/jdk/8u401-b10/4d245f941845490c91360409ecffb3b4/jdk-8u401-macosx-x64.dmg
+  # url "https://download.oracle.com/otn/java/jdk/#{java_release}-b10/4d245f941845490c91360409ecffb3b4/jdk-#{java_release}-macosx-#{arch}.dmg"
+  # Mirror URL, thanks to https://gist.github.com/wavezhang/ba8425f24a968ec9b2a8619d7c2d86a6
+  url "https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java8/java#{java_release}/jdk/jdk-#{java_release}-macosx-#{arch}.dmg"
+  name "Oracle Java Standard Edition Development Kit (JDK SE) version 8"
+  # homepage "https://www.oracle.com/technetwork/java/javase/overview/index.html"
+  homepage "https://www.oracle.com/java/technologies/downloads/#java8-mac" # More direct page (linked from the previous page)
+
+  livecheck do
+    # Uses the Security Baseline page, recommended by Oracle to list all the latest versions
+    url "https://javadl-esd-secure.oracle.com/update/baseline.version"
+    strategy :page_match
+    regex(/(1.8.*)/i)
+  end
+  
+  pkg "JDK #{version.minor} Update #{java_update}.pkg"
+  # pkg "JDK " + java_release.sub("u"," Update ") + ".pkg" # e.g. "JDK 8 Update 172.pkg"
+
+  # uninstall pkgutil: "com.oracle.jdk#{java_release}"
+  uninstall pkgutil:   [
+                         "com.oracle.jdk#{java_release}",
+                         'com.oracle.jre',
+                       ],
+            launchctl: [
+                         'com.oracle.java.Helper-Tool',
+                         'com.oracle.java.Java-Updater',
+                       ],
+            quit:      [
+                         'com.oracle.java.Java-Updater',
+                         'net.java.openjdk.cmd', # Java Control Panel
+                       ],
+            delete:    [
+                         '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin',
+                         "/Library/Java/JavaVirtualMachines/jdk-#{version.major_minor}.jdk/Contents",
+                         '/Library/PreferencePanes/JavaControlPanel.prefPane',
+                         '/Library/Java/Home',
+                       ],
+            rmdir:     "/Library/Java/JavaVirtualMachines/jdk-#{version.major_minor}.jdk" 
+  #         rmdir:     "/Library/Java/JavaVirtualMachines/jdk#{version.split('-')[0]}.jdk" # e.g. 1.8.0_172 for 1.8.0_172-b10
+
+  zap trash: [
+               '~/Library/Application Support/Java/',
+               '~/Library/Application Support/Oracle/Java',
+               '~/Library/Caches/com.oracle.java.Java-Updater',
+               '~/Library/Caches/Oracle.MacJREInstaller',
+               '~/Library/Caches/net.java.openjdk.cmd',
+               '~/Library/Preferences/com.oracle.java.Java-Updater.plist',
+               '~/Library/Preferences/com.oracle.java.JavaAppletPlugin.plist',
+               '~/Library/Preferences/com.oracle.javadeployment.plist',
+             ],
+      rmdir: '~/Library/Application Support/Oracle/'
+
+  caveats do
+    license "https://www.oracle.com/technetwork/java/javase/terms/license/javase-license.html"
+  end
+end
