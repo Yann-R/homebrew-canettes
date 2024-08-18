b06ce032fd513b9d2c2dcdff0e6b3720f1fb1159
Yann-R <yann.ricquebourg@irisa.fr>
2024-08-18 22:37:51 +0200

    Bump slackdump to version 2.5.10

--- Formula/slackdump.rb
+++ Formula/slackdump.rb
@@ -3,12 +3,12 @@ class Slackdump < Formula
 
   desc "Slack Dumper for messages, users, files and emojis"
   homepage "https://github.com/rusq/slackdump"
-  version "2.5.9"
+  version "2.5.10"
   url "https://github.com/rusq/slackdump/releases/download/v#{version}/slackdump_macOS_#{arch}.tar.gz"
   if Hardware::CPU.arm?
-    sha256 "5582e2d70cce4ca15d25252ebd4390ee5530050898569ea8bbeb1ccb7567cca9"
+    sha256 "df04dade95169b9a8b5c013c7eb288f702409141e5621caf36ddec4bda3d6489"
   else # Hardware::CPU.intel?
-    sha256 "e4601af3be4203a01a079a6c9ec3de410b292291da13df43efe98b5cb72c2b96"
+    sha256 "846566c9ad662ccfd386dc6070cbfb51390076cb139a0b8581168413f674baeb"
   end
   license "GPL-3.0-only"
 
