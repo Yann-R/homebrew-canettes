687818bcc4e40115638d354cdd4a272ce4e69d5f
Yann-R <yann.ricquebourg@irisa.fr>
2024-10-23 19:00:42 +0200

    Bump solstice to version 6.2

--- Casks/solstice.rb
+++ Casks/solstice.rb
@@ -1,6 +1,6 @@
 cask "solstice" do
-  version "5.3.4"
-  sha256 "0c1941756960741de95de3147673782c462ebeeabcf1ade7a9f64e07f3e7262e"
+  version "6.2"
+  sha256 "145a06cd939a32ac00baaa9f8f152153dc84c81a9291acefe189a3bfd4f1f84c"
 
   url "https://www.mersive.com/files/41693/"
   name "Mersive Solstice"
@@ -8,14 +8,8 @@ cask "solstice" do
   homepage "https://www.mersive.com/download/"
 
   livecheck do
-    url :homepage
-    regex(%r{/files/(?<num>\d+)/.+Download for macOS Laptop}mi) # multi-lines case-insensitive
-    # e.g. <a href="/files/41693/" class="elementor-button-link elementor-button elementor-size-sm" role="button">
-    #      [...]<span class="elementor-button-text">Download for macOS Laptop (dmg)</span>
-    strategy :page_match do |page, regex|
-      match = page.match(regex)
-      "#{match[:num]}" == "41693" ? version : "new"
-    end
+    url :stable # Note: URL /files/41693/ remained the same over versions
+    strategy :header_match
   end
 
   app "Mersive Solstice.app"
