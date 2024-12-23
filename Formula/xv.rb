f0681b0cd7d1cc1183fd34461db98bc4d2b565ca
Yann-R <yann.ricquebourg@irisa.fr>
2024-12-23 21:32:41 +0100

    Bump xv to version 6.0.2

--- Formula/xv.rb
+++ Formula/xv.rb
@@ -1,72 +1,40 @@
 class Xv < Formula
   desc "Interactive image manipulation program for the X Window System"
-  homepage "http://www.trilon.com/xv/"
-  # David Griffith's repository is one of the most interesting sources for John Bradley's xv:
-  # - integrates Greg Roelofs's jumbo patch => http://www.gregroelofs.com/greg_xv.html
-  # - and adds many more => https://gitlab.com/DavidGriffith/xv/-/issues/4
-  url "https://gitlab.com/DavidGriffith/xv.git",
-      revision: "c704ea045d1243a19d97ffd0eb74997c413b89d6",
-      shallow:  false
-  version "3.10a-20190925@224108Z" # date of the last patch on original version 3.10a
-  # license "shareware for personnal use only"
-  head "https://gitlab.com/DavidGriffith/xv.git"
+  # homepage "http://www.trilon.com/xv/"            # Discontinued
+  homepage "https://github.com/jasper-software/xv"  # Revival
+  url "https://github.com/jasper-software/xv/releases/download/v6.0.2/xv-3.10a-js-6.0.2.tar.gz"
+  sha256 "bcc36a67639fdd55f93c59654bd91c31bac7fa071fc1ed6f5352e1408d535482"
+  # license "shareware for personal use only"
+  head "https://github.com/jasper-software/xv.git", branch: "main"
 
   livecheck do
-    url "https://gitlab.com/DavidGriffith/xv.atom"
-    regex(%r{<updated>(?<year>.+)-(?<month>.+)-(?<day>.+)T(?<hr>.+):(?<min>.+):(?<sec>.+)</updated>}i)
-    # e.g. <updated>2019-09-25T22:41:08Z</updated>
-    strategy :page_match do |page, regex|
-      match = page.match(regex)
-      "3.10a-#{match[:year]}#{match[:month]}#{match[:day]}@#{match[:hr]}#{match[:min]}#{match[:sec]}"
-    end
+    url :stable
+    strategy :github_latest
   end
 
+  depends_on "cmake" => :build
   depends_on "jasper"
   depends_on "jpeg"
+  depends_on "libice" # Implied by libxt
   depends_on "libpng"
+  depends_on "libsm"  # Implied by libxt
   depends_on "libtiff"
-  depends_on "libx11"
+  depends_on "libx11" # Implied by libxt
   depends_on "libxrandr"
   depends_on "libxt"
+  depends_on "ghostscript" => :build # for ps2pdf
   depends_on "webp"
   uses_from_macos "zlib"
 
   def install
-    # Adapt the source files for macOS
-    # inreplace "vdcomp.c", "__DARWIN__", "__APPLE__"
-    inreplace "xv.h", "extern char *sys_errlist[];", ""
-    inreplace "xv.h", "#include <stdio.h>", "#include <stdio.h>\n#include <limits.h>"
-    inreplace "xvjp2k.c", "jas_memdump(FILE *fp,void *data,size_t len)",
-              "jas_memdump(FILE *fp,const void *data,size_t len)"
-    inreplace "xvmisc.c", "int           keepsize;",
-              "int           keepsize;\nint           userspec;"
+    system "cmake", "-S", ".", "-B", "tmp_cmake", *std_cmake_args # Build makefile
+    system "cmake", "--build", "tmp_cmake", "--target", "install" # Build & Install
 
-    # Adapt the source files for brewing
-    inreplace "xv.h", "#define REVDATE   \"version 3.10a-jumboFix+Enh of 20081216 (interim!)\"",
-              "#define REVDATE   \"version 3.10a-jumboFix+Enh of #{version.to_str[6,13]} (David Griffith\'s release)\""
-    inreplace "xv.h", "#define VERSTR    \"3.10a-20081216\"",
-              "#define VERSTR    \"3.10a-#{version.to_str[6,8]}\""
-    inreplace "Makefile", "/usr/X11R6/lib", "#{HOMEBREW_PREFIX}/lib"
-    # To add X11 include as last, avoids imposing its embedded png includes
-    inreplace "Makefile", '-DXVEXECPATH=\"$(LIBDIR)\"',
-              '-DXVEXECPATH=\"$(LIBDIR)\"' + " -I#{HOMEBREW_PREFIX}/include"
-    inreplace "Makefile", "TIFFDIR = /usr", "TIFFDIR = #{HOMEBREW_PREFIX}"
-    inreplace "Makefile", "JPEGDIR = /usr", "JPEGDIR = #{HOMEBREW_PREFIX}"
-    inreplace "Makefile", "PNGDIR = /usr", "PNGDIR = #{HOMEBREW_PREFIX}"
-    # inreplace "Makefile", "ZLIBDIR = /usr", "ZLIBDIR = #{HOMEBREW_PREFIX}" # Uses macOS version
-    # To enable Jpeg 2000
-    # inreplace "Makefile", "#JP2K    = -DDOJP2K", "JP2K    = -DDOJP2K" # TODO: re-enable Jpeg 2000 later
-    inreplace "Makefile", "#JP2KDIR = /usr/local/lib", "JP2KDIR = #{HOMEBREW_PREFIX}"
-    inreplace "Makefile", "#JP2KINC = -I$(JP2KDIR)", "JP2KINC = -I$(JP2KDIR)/include"
-    inreplace "Makefile", "#JP2KLIB = -L$(JP2KDIR) -ljasper", "JP2KLIB = -L$(JP2KDIR)/lib -ljasper"
-    # To set install directory (Makefile notes to update code for that with old K&R compilers)
-    inreplace "Makefile", "PREFIX = /usr/local", "PREFIX = #{prefix}"
-    # To avoid big ps documentation (but keep pdf version)
-    inreplace "Makefile", "docs/xvdocs.ps", ""
-    inreplace "Makefile", "$(DESTDIR)$(DOCDIR)/xvdocs.ps", ""
-    
-    # Make & Install
-    system "make", "install"
+    man5.install Dir["#{doc}/formats/*.5"]      # Note: then *.5 removed from #{doc}
+    Utils::Gzip.compress *Dir["#{man}/**/*.*"]  # All dirs, recursively
+    system "ps2pdf", "#{doc}/manuals/xvtitle.ps", "#{doc}/manuals/xvtitle.pdf"
+    File.unlink "#{doc}/manuals/xvdocs.ps"  if File.exist? "#{doc}/manuals/xvdocs.pdf"
+    File.unlink "#{doc}/manuals/xvtitle.ps" if File.exist? "#{doc}/manuals/xvtitle.pdf"
   end
 
   test do
