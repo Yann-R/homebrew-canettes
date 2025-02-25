e7a34775dfe36661b41bf5c11ada9b31957e29b7
Yann-R <yann.ricquebourg@irisa.fr>
2025-02-25 20:23:23 +0100

    Update toshiba-mono-mfp.rb

--- Casks/toshiba-mono-mfp.rb
+++ Casks/toshiba-mono-mfp.rb
@@ -2,7 +2,7 @@ cask "toshiba-mono-mfp" do
   version "7.117.3"
   sha256 "49049891f55eb61e177b4f97cd106339f395134e98c7d8e30ca388d6c3b1e054"
 
-  url "https://www.toshibatec.eu/publicsite-service/resource/download/pseu/en/81c15239-1ebb-44fb-a9df-169ca32cc9db/7ca8208c71dc180b271cba91a1f0b7c4/TOSHIBA%20e-STUDIO%20MacPPD%20(10.12-latest)%20v#{version}.zip"
+  url "https://downloads.toshibatec.eu/publicsite-service/resource/download/pseu/en/81c15239-1ebb-44fb-a9df-169ca32cc9db/7ca8208c71dc180b271cba91a1f0b7c4/TOSHIBA%20e-STUDIO%20MacPPD%20(10.12-latest)%20v#{version}.zip"
   name "Toshiba MonoMFP Drivers"
   desc "Drivers (Monochrome only) for Multi-Function Printer Toshiba e-STUDIO Series"
   homepage "https://www.toshibatec.eu/products/multifunctional-systems-and-printers/"
