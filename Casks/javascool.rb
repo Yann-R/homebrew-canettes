832b1435aefd689699cd80116dbe7a25fec72a12
Yann-R <yann.ricquebourg@irisa.fr>
2025-04-12 22:07:15 +0200

    Bump javascool to version 4.0.1329 (2017-06-10)

--- Casks/javascool.rb
+++ Casks/javascool.rb
@@ -1,14 +1,15 @@
 cask "javascool" do
-  version "4"
-  sha256 "051190db7b54f583011c3ffe2b1c4e3063db7d47877f2dbdc9044d248ed4c1b4"
+  version "4.0.1329,2017-06-10"
+  sha256 "7553816eba2c2a65117a0be3d98168b8e176e7782b9984be7c9e10fddc09f96c"
 
   # url "http://javascool.gforge.inria.fr/javascool-proglets.jar" # Official no longer available
-  url  "https://web.archive.org/web/20170418223738/http://javascool.gforge.inria.fr/javascool-proglets.jar"
+  url  "https://web.archive.org/web/20170621094236/http://javascool.gforge.inria.fr:80/javascool-proglets.jar"
   name "Java's Cool 4"
   # see description "https://www.enseignementsup-recherche.gouv.fr/ressources-pedagogiques/notice/view/INRIA-Javascool"
   desc "Java-based Macro-language and ressources to learn Programming and Algorithms"
   # homepage "http://javascool.gforge.inria.fr" # Official no longer available
-  homepage "https://wiki.inria.fr/wikis/sciencinfolycee/index.php?search=javascool"
+  homepage "https://web.archive.org/web/20190319204320/http://javascool.gforge.inria.fr/index.php?page=run"
+  # homepage "https://wiki.inria.fr/wikis/sciencinfolycee/index.php?search=javascool"
 
   shimscript = "#{staged_path}/javascool.sh"
   app "javascool-proglets.jar"
