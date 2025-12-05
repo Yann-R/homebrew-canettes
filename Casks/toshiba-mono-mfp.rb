9748ab26c3bbf3a2c8fd83ed8897181692e00a86
Yann-R <yann.ricquebourg@irisa.fr>
2025-12-05 12:21:06 +0100

    Bump toshiba-mono-mfp to version 7.119.4.0,21840

--- Casks/toshiba-mono-mfp.rb
+++ Casks/toshiba-mono-mfp.rb
@@ -1,34 +1,34 @@
 cask "toshiba-mono-mfp" do
-  version "7.117.3"
-  sha256 "49049891f55eb61e177b4f97cd106339f395134e98c7d8e30ca388d6c3b1e054"
+  version "7.119.4.0,21840"
+  sha256 "ee57f969fae215485755a5dfb7e396182f76f9977a05568f9ac7c66643d9432a"
 
-  url "https://downloads.toshibatec.eu/publicsite-service/resource/download/pseu/en/81c15239-1ebb-44fb-a9df-169ca32cc9db/7ca8208c71dc180b271cba91a1f0b7c4/TOSHIBA%20e-STUDIO%20MacPPD%20(10.12-latest)%20v#{version}.zip"
+  url "https://business.toshiba.com/downloads/KB/f1Ulds/#{version.csv.second}/TOSHIBA_MonoMFP.dmg.gz"
   name "Toshiba MonoMFP Drivers"
-  desc "Drivers (Monochrome only) for Multi-Function Printer Toshiba e-STUDIO Series"
-  homepage "https://www.toshibatec.eu/products/multifunctional-systems-and-printers/"
+  desc "Drivers (Duplex mode) for Multi-Function Printer Toshiba e-STUDIO Series"
+  homepage "https://business.toshiba.com/product/e-studio9029a#downloads"
 
   livecheck do
-    url "https://www.toshibatec.eu/products/multifunctional-systems-and-printers/e-studio400ac/"
-    strategy :page_match
-    regex(/TOSHIBA e-STUDIO MacPPD.*v(\d+(?:\.\d+)*)/i)
-    # e.g. TOSHIBA e-STUDIO MacPPD(10.7-) v7.111
-  end
+    url "https://business.toshiba.com/support/downloads/GetDownloads.jsp?model=e-studio9029a"
+    strategy :json do |json|
+      json["drivers"]&.map do |item|
+        next unless item["name"]&.include?("MacDM")
 
-  # Installs the 1st pkg for Monochrome MFP (in double-sided mode)
-  container nested: "OSX10.12-latest/2-sided_default/TOSHIBA_MonoMFP.dmg.gz"
+        id = item["id"]
+        version = item["versionName"]
+        #next if id.blank? || version.blank?
+
+        "#{version},#{id}"
+      end
+    end
+  end
 
   pkg "TOSHIBA MonoMFP.pkg"
 
-#  # No longer possible to handle a second container
-#  postflight do
-#    # Installs the 2nd pkg for Color MFP (in double-sided mode)
-#    container nested: "OSX10.12-latest/2-sided_default/TOSHIBA_ColorMFP.dmg.gz"
-#    pkg "TOSHIBA ColorMFP.pkg"
-#  end
-#
-#  uninstall_postflight do
-#    uninstall pkgutil: "com.toshiba.pde.x7.colormfp"
-#  end
+  uninstall pkgutil: "com.toshiba.pde.x7.monomfp",
+            delete:  [
+              "/Library/Printers/PPDs/Contents/Resources/TOSHIBA_MonoMFP*.gz",
+              "/Library/Printers/toshiba",
+            ]
 
-  uninstall pkgutil: "com.toshiba.pde.x7.monomfp"
+  # No zap stanza required
 end
