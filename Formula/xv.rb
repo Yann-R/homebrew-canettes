e433e6a9eaf61cce061222d59aeb08284bf78a6b
Yann-R <yann.ricquebourg@irisa.fr>
2025-12-05 16:35:27 +0100

    Bump xv to version 6.1.0

--- Formula/xv.rb
+++ Formula/xv.rb
@@ -1,9 +1,9 @@
 class Xv < Formula
   desc "Interactive image manipulation program for the X Window System"
-  # homepage "http://www.trilon.com/xv/"            # Discontinued
-  homepage "https://github.com/jasper-software/xv"  # Revival
-  url "https://github.com/jasper-software/xv/releases/download/v6.0.4/xv-3.10a-js-6.0.4.tar.gz"
-  sha256 "af4fbca1dc90d93446e2512349809e4c6608fb82b4a6900166dd970f3b2861d8"
+  # homepage "http://www.trilon.com/xv/"          	# Discontinued
+  homepage "https://github.com/jasper-software/xv"	# Revival
+  url "https://github.com/jasper-software/xv/releases/download/v6.1.0/xv-3.10a-js-6.1.0.tar.gz"
+  sha256 "bfd0df563a7d5e6632fd22a4789d6a2e5ec24c83027d058a563eebc6dc74ca9c"
   # license "shareware for personal use only"
   head "https://github.com/jasper-software/xv.git", branch: "main"
 
@@ -13,6 +13,7 @@ class Xv < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "ghostscript" => :build # for ps2pdf
   depends_on "jasper"
   depends_on "jpeg"
   depends_on "libice" # Implied by libxt
@@ -22,7 +23,6 @@ class Xv < Formula
   depends_on "libx11" # Implied by libxt
   depends_on "libxrandr"
   depends_on "libxt"
-  depends_on "ghostscript" => :build # for ps2pdf
   depends_on "webp"
   uses_from_macos "zlib"
 
@@ -32,7 +32,7 @@ class Xv < Formula
 
     # Following man5 install disabled (kept in doc) due to man conflict with netpbm
     # man5.install Dir["#{doc}/formats/*.5"]    # Move *.5 from doc to man pages
-    Utils::Gzip.compress *Dir["#{man}/**/*.*"]  # All dirs, recursively
+    Utils::Gzip.compress(*Dir["#{man}/**/*.*"]) # All dirs, recursively
     system "ps2pdf", "#{doc}/manuals/xvtitle.ps", "#{doc}/manuals/xvtitle.pdf"
     File.unlink "#{doc}/manuals/xvdocs.ps"  if File.exist? "#{doc}/manuals/xvdocs.pdf"
     File.unlink "#{doc}/manuals/xvtitle.ps" if File.exist? "#{doc}/manuals/xvtitle.pdf"
